81aae2eb56bf9e397a2f559af2f76e8f4d929d4b
Gabriel Koch <160144370+Gabriel-Koch-Nunes@users.noreply.github.com>
2024-06-20 09:34:38 -0300

    Create ItemVenda.java

---
+++ ItemVenda.java
@@ -0,0 +1,21 @@
+public class ItemVenda {
+    private Produto produto;
+    private int quantidade;
+
+    public ItemVenda(Produto produto, int quantidade){
+        this.produto=produto;
+        this.quantidade=quantidade;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public Produto getProduto() {
+        return produto;
+    }
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+}
